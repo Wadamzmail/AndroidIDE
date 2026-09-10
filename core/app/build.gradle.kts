3ef0e97223d12541e652ca3aeb1d475f6a9343c7
Wadamzmail <alksndrmetwakil@gmail.com>
2026-09-10 13:44:51 +0200

    integrate idecfgs

--- core/app/build.gradle.kts
+++ core/app/build.gradle.kts
@@ -215,6 +215,8 @@ dependencies {
 	implementation(projects.subprojects.projectModels)
 	implementation(projects.subprojects.gitCore)
 	implementation(projects.subprojects.layoutEditor)
+	implementation(projects.subprojects.ideConfigurations)
+	implementation(projects.subprojects.ideProvider)
 
     // This is to build the tooling-api-impl project before the app is built
     // So we always copy the latest JAR file to assets
--- core/app/src/main/java/dev/mutwakil/androidide/preferences/rootPrefExts.kt
+++ core/app/src/main/java/dev/mutwakil/androidide/preferences/rootPrefExts.kt
@@ -40,6 +40,7 @@ class ConfigurationPreferences(
     addPreference(BuildAndRunPreferences())
     addPreference(TermuxPreferences())
     addPreference(GitPreferencesScreen())
+    addPreference(ideConfigurations)
   }
 }
 
